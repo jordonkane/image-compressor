5613dd6d719636b18301a964260d69d860704c3d
jordonkane <kanejord99@outlook.com>
2024-09-25 02:15:13 -0400

    Initialize base project

---
+++ src/main/java/com/kanej/imagecompressor/JpegCompressor.java
@@ -0,0 +1,10 @@
+package com.kanej.imagecompressor;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class JpegCompressor {
+
+}
