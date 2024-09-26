e45cdf9d762c56598a3b72e174bd921d4c4e5ab6
jordonkane <kanejord99@outlook.com>
2024-09-26 15:12:32 -0400

    Implement ImageUtils - divide images into 8x8 Blocks
    divide the images into 8x8 blocks

--- src/main/java/com/kanej/imagecompressor/ImageUtils.java
+++ src/main/java/com/kanej/imagecompressor/ImageUtils.java
@@ -1,4 +1,14 @@
 package com.kanej.imagecompressor;
 
 public class ImageUtils {
+
+    public static double[][] extractBlock(double[][] channel, int startX, int startY) {
+        double[][] block = new double[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                block[i][j] = channel[startY + i][startX + j];
+            }
+        }
+        return block;
+    }
 }
