e410b659e46ec6f65c27eafa289cb9545de62112
jordonkane <kanejord99@outlook.com>
2024-09-25 20:00:05 -0400

    Add core project components

---
+++ src/main/java/com/kanej/imagecompressor/DCT.java
@@ -0,0 +1,4 @@
+package com.kanej.imagecompressor;
+
+public class DCT {
+}
--- src/main/java/com/kanej/imagecompressor/JpegCompressor.java
+++ src/main/java/com/kanej/imagecompressor/JpegCompressor.java
@@ -2,9 +2,12 @@ package com.kanej.imagecompressor;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class JpegCompressor {
 
+    public void compress(String inputPath, String outputPath) throws Exception {
+
+        // TODO: Save compressed data to output JPEG file (future step)
+    }
 }
